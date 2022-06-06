6aadf18ffe176cdb9f62b3995a639d6c983773c1
gouzal <larbi-inco@live.fr>
2022-06-05 20:33:14 -0400

    fix bug in user insert sql query

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
@@ -1,13 +1,11 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
-import com.udacity.jwdnd.course1.cloudstorage.model.File;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import java.util.List;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
@@ -26,7 +24,7 @@ public interface UserMapper {
     @Update("UPDATE  FILES SET notetitle='#{noteTitle}', notedescription='#{noteDescription}' WHERE noteid = #{noteId}")
     List<User> update(User user);
 
-    @Insert("INSERT INTO USERS(firstname,lastname,username,password,salt) VALUES('#{firstname}','#{lastname}','#{username}','#{password}','#{salt}')")
+    @Insert("INSERT INTO USERS(firstname,lastname,username,password,salt) VALUES(#{firstname},#{lastname},#{username},#{password},#{salt})")
     @Options(useGeneratedKeys = true, keyProperty = "userId")
     int insert(User user);
 
