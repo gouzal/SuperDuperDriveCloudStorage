639e88e16b9ffc1d8b4c166e28f4f86d7df49529
gouzal <larbi-inco@live.fr>
2022-07-26 22:36:10 -0400

    remove implicit scope

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/AbstractPersistenceService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/AbstractPersistenceService.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 public interface AbstractPersistenceService<T> /*permits NoteService, CredentialService, FileService, UserService*/ {
 
-    public T find(long id);
+    T find(long id);
 
     int update(T obj);
 
